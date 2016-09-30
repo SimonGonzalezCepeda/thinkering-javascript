54c373975261bfc8495d0b82fa890cab5f198c98
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-09-30 16:49:17 +0200

    Reviewinw namespaces

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-// VARIABLES GLOBALS: LA PESTE: XOCX
-$name = 'Sergi';
-function fire(bool) {
-    console.log("Hola " + $name);
+//NAMESPACES
 
-    if (bool) {
-        foo = bar;
+//VECTOR
 
-        console.log(foo);
-    } else {
-        console.log(foo);
+var Person = [
+
+    {
+        'name' : 'Sylver',
+        'sn1'  : 'Shine',
+        'age'  : 28
+    },
+    {
+        'name' : 'Mimi',
+        'sn1'  : 'Pink',
+        'age'  : 26
     }
-}
 
-fire(true);
+];
+
+
+//JSON
+
+var Persona = {
+    'name' : 'Sylver',
+    'sn1'  : 'Shine',
+    'age'  : 28
+};
+
+//JQuery $
+
+var Person = {};
+
+Person.name = 'Sylver';
+
+Person.hello = function() {
+    console.log("Hi! " + Person.name);
+};
